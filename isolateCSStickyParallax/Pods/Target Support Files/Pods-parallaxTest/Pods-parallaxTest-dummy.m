d1f88ec71284eaf6df9bc88a833c8eecf790e73e
Terry Bu <ktbucifer@gmail.com>
2014-11-14 13:14:23 -0500

    fix

--- isolateCSStickyParallax/Pods/Target Support Files/Pods-parallaxTest/Pods-parallaxTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_parallaxTest : NSObject
-@end
-@implementation PodsDummy_Pods_parallaxTest
-@end
--- isolateVGParallax/Pods/Target Support Files/Pods-isolateVGParallax-VGParallaxHeader/Pods-isolateVGParallax-VGParallaxHeader-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_isolateVGParallax_VGParallaxHeader : NSObject
-@end
-@implementation PodsDummy_Pods_isolateVGParallax_VGParallaxHeader
-@end
